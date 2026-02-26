000423e90c04cdd326e94fe6cadee99eca166b60
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-02-26 13:07:04 -0800

    1404 solve with string manipulation

--- 1404_Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One.py
+++ 1404_Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One.py
@@ -1,17 +1,44 @@
+import copy
 class Solution:
     def numSteps(self, s: str) -> int:
-        n = int(s,2)
         count = 0
-        while n != 1:
-            if n%2 == 0:
-                n = n//2
+        while s != "1":
+            if s[-1] == "0":
+                s = s[0:-1]
             else:
-                n = n+1
+                s = increment(s)
             count += 1
         return count
+
+def increment(s):
+    if s[-1] == "0":
+        return s[0:-1] + "1"
+    
+    i = len(s)-1
+    foundZero = False
+    while i > 0:
+        char = s[i]
+        if char == "0":
+            foundZero = True
+            break
+        i -= 1
+
+    if foundZero:
+        return s[0:i] + "1" + "0"*(len(s)-i-1)
+    else:
+        return "1" + "0"*len(s)
 
 
 
 mySol = Solution()
-ans = mySol.numSteps("1")
+ans = mySol.numSteps("1101")
 print(ans)
+
+# i = 0
+# s = "1"
+# while i < 128:
+#     print(s)
+#     input()
+#     s= increment(s)
+
+    
