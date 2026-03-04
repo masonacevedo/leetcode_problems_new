7e88001b18b425de3a3dc5abee09f7b533df78a6
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-03-04 09:43:03 -0800

    169 - cute mathematical solution!

--- 169_Majority_Element.py
+++ 169_Majority_Element.py
@@ -1,9 +1,3 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        freqs = {}
-        for num in nums:
-            if num in freqs:
-                freqs[num] += 1
-            else:
-                freqs[num] = 1
-        return max(freqs.items(), key=lambda item: item[1])[0]
+        return sorted(nums)[len(nums)//2]
