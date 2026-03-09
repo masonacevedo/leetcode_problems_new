45a0b9fa7811ac5426b2a4a34deec4172fc512e1
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-03-09 11:40:15 -0700

    196 working!!

--- 196_Delete_Duplicate_Emails.sql
+++ 196_Delete_Duplicate_Emails.sql
@@ -4,4 +4,14 @@ INSERT INTO Person (email) VALUES ("john@example.com");
 INSERT INTO Person (email) VALUES ("bob@example.com");
 INSERT INTO Person (email) VALUES ("john@example.com");
 
+DELETE FROM Person 
+WHERE id NOT IN
+    (
+        SELECT * 
+        FROM (
+            SELECT MIN(id) FROM Person
+            GROUP BY email
+        ) as temp
+    );
+
 SELECT * FROM Person;
