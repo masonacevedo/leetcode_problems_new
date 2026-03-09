33f79a1bffc38ac987176261c790a1ebb3d810ea
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-03-08 21:33:12 -0700

    518 DP solution done. Crazy how slowing down actually helps you speed up!

--- 518_Coin_Change_II.py
+++ 518_Coin_Change_II.py
@@ -5,45 +5,78 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         if amount == 0:
             return 1
-        return self.changeHelper(amount, sorted(coins), {})
+        return self.changeHelper(amount, sorted(coins))
     
-    def changeHelper(self, amount: int, coins: List[int], memo) -> int:
+    # DP SOLUTION:
+    # columns indexed by what coins are used,
+    # rows indexed by amount.
+    # i.e.
+    # [waysToMake 37 with 1st coin, waysToMake 37 with 2nd coin, waysToMake 37 with 3rd coin... waysToMake 37 with all coins, ]
+    # [waysToMake 38 with 1st coin, waysToMake 38 with 2nd coin, waysToMake 38 with 3rd coin... waysToMake 38 with all coins, ]
+    # we build this solution by going down through an entire column first (the 1st column, with only one coin)
+    # then go from there. 
+    def changeHelper(self, amount: int, coins: List[int]) -> int:
+        DP = [[None for _ in range(0, len(coins))] for _ in range(0, amount+1)]
+        for col in range(0, len(coins)):
+            for row in range(0, amount+1):
+                ans = calculateNumWays(row, col, row, coins, DP)
+                DP[row][col] = ans
+            # prettyPrint(DP)
+            # print()
+            # input()
 
-        if len(coins) == 0:
-            return -1
         
-        k = (amount, tuple(coins))
-        if k in memo:
-            return memo[k]
+        return DP[amount][-1]
 
-        minCoin = coins[0]
-        if amount < minCoin:
-            return -1
-        elif amount == minCoin:
+
+def calculateNumWays(amount, col, row, coins, DP):
+    if row == 0:
+        return 1
+    elif col == 0:
+        coinToUse = coins[0]
+        if (amount % coinToUse) == 0:
             return 1
-        
-        maxTimesUsed = amount//minCoin
+        else:
+            return 0
+    
+    ans = 0
+    coinToUse = coins[col]
 
-        ans = 0
-        for i in range(0, maxTimesUsed+1):
-            remainingAmount = amount - (minCoin*i)
-            
-            if remainingAmount == 0:
-                ans += 1
-            else:
-                recursiveCall = self.changeHelper(remainingAmount, coins[1:], memo)
-                if recursiveCall != -1:
-                    ans += recursiveCall
-            
-        memo[k] = ans
-        return ans
+    maxTimesTouse = amount//coinToUse
+
+    for i in range(0, maxTimesTouse+1):
+        amountToMake = amount - (coinToUse * i)
+        if amountToMake < 0:
+            pass
+        else:
+            # print("amountToMake:", amountToMake)
+            # print("col-1:", col-1)
+            # prettyPrint(DP)
+            # input()
+
+            ans += DP[amountToMake][col-1]
+
+    return ans
 
 
+
+def prettyPrint(rows):
+    for row in rows:
+        ans = "["
+        for entry in row:
+            ans += str(entry) + ", "
+        ans += "]"
+        print(ans)
 
 s = Solution()
 
 amount = 500
 coins = [3,5,7,8,9,10,11]
+# amount = 4
+# coins = [1, 2, 3]
+# 10, [1,2] -> answer should be 6
+# 12 [1,2] -> answer should be 7
+# 4, 1, 2, 3 => answer should be: 1 + 1 + 1 + 1 = 4
 
 ans = s.change(amount, coins)
 print("ans:", ans)
