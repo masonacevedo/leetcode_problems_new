3bb51b3e64f165fccb9caa70b94cc8f60effbde9
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-03-08 23:02:17 -0700

    322 DP solution done!

--- 322_Coin_Change.py
+++ 322_Coin_Change.py
@@ -2,38 +2,38 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        return self.coinChangeHelper(coins, amount, {})
+        return self.coinChangeHelper(coins, amount)
 
-    def coinChangeHelper(self, coins: List[int], amount: int, memo) -> int:
-        if amount in memo:
-            return memo[amount]
+    def coinChangeHelper(self, coins: List[int], amount) -> int:
+        DP = [0]
 
-        if amount < 0:
-            return -1
-        elif amount == 0:
-            return 0
-        
-        
-        # candidates = []
-        bestSoFar = float('inf')
-        for coin in coins:
-            amountToMake = amount - coin
-            recursiveCall = self.coinChangeHelper(coins, amountToMake, memo)
-            if recursiveCall == -1:
-                pass
-            else:
-                bestSoFar = min(bestSoFar, 1 + recursiveCall)
-        
-        # print("amount:", amount)
-        # print("candidates:", candidates)
-        # input()
-        if bestSoFar == float('inf'):
-            memo[amount] = -1
-            return -1
-        memo[amount] = bestSoFar
-        return bestSoFar
+        # In this DP table, we'll want entry i to be the amount of coins
+        # needed to make i amount of money! 
 
-coins = [1,2, 5]
+        for i in range(1, amount+1):
+            ans = minCoinsNeeded(coins, i, DP)
+            DP.append(ans)
+            
+
+        return DP[-1]
+
+def minCoinsNeeded(coins, amount, DP):
+    if amount == 0:
+        return 0
+    bestSoFar = float('inf')
+    for coin in coins:
+        amountToMake = amount - coin
+        if amountToMake >= 0:
+            DPValue = DP[amountToMake]
+            if DPValue != -1:
+                bestSoFar = min(bestSoFar, 1 + DPValue)
+
+    if bestSoFar == float('inf'):
+        return -1
+    
+    return bestSoFar
+
+coins = [1, 2, 5, 99]
 amount = 100
 
 s = Solution()
