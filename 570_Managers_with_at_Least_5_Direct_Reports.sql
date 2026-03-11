81c37815c635fc3bf7f74cd709b586279d685aa7
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-03-11 10:33:28 -0700

    570 done!

---
+++ 570_Managers_with_at_Least_5_Direct_Reports.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF Exists Employee;
+Create table If Not Exists Employee (id int, name varchar(255), department varchar(255), managerId int);
+Truncate table Employee;
+insert into Employee (id, name, department, managerId) values ('101', 'John', 'A', NULL);
+insert into Employee (id, name, department, managerId) values ('102', 'Dan', 'A', '101');
+insert into Employee (id, name, department, managerId) values ('103', 'James', 'A', '101');
+insert into Employee (id, name, department, managerId) values ('104', 'Amy', 'A', '101');
+insert into Employee (id, name, department, managerId) values ('105', 'Anne', 'A', '101');
+insert into Employee (id, name, department, managerId) values ('106', 'Ron', 'B', '101');
+
+SELECT name
+FROM Employee WHERE 
+id in (
+    SELECT managerId FROM (
+        SELECT managerId, COUNT(*) as directReportCount
+        FROM Employee
+        GROUP BY managerId
+        HAVING directReportCount >= 5
+    ) as managerIds
+);
