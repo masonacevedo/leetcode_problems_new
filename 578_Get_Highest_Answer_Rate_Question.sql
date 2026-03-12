24e464b988d93b3868d8a8e37a4c93c5aefa212f
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-03-11 23:59:07 -0700

    578 done

--- 578_Get_Highest_Answer_Rate_Question.sql
+++ 578_Get_Highest_Answer_Rate_Question.sql
@@ -4,11 +4,9 @@ insert into SurveyLog (id, action, question_id, answer_id, q_num, timestamp) val
 insert into SurveyLog (id, action, question_id, answer_id, q_num, timestamp) values ('5', 'answer', '285', '124124', '1', '124');
 insert into SurveyLog (id, action, question_id, answer_id, q_num, timestamp) values ('5', 'show', '369', NULL, '2', '125');
 insert into SurveyLog (id, action, question_id, answer_id, q_num, timestamp) values ('5', 'skip', '369', NULL, '2', '126');
-insert into SurveyLog (id, action, question_id, answer_id, q_num, timestamp) values ('6', 'show', '369', NULL, '2', '127');
-insert into SurveyLog (id, action, question_id, answer_id, q_num, timestamp) values ('6', 'answer', '369', '574803', '2', '128');
 
 
-SELECT * FROM
+SELECT qid as survey_log FROM
 (SELECT showTable.question_id AS qid, answerCount/showCount as answerRate FROM
     (SELECT question_id, action, COUNT(*) as showCount FROM SurveyLog
     GROUP BY 
@@ -21,3 +19,6 @@ SELECT * FROM
     HAVING action="answer") as answerTable
     ON
     showTable.question_id=answerTable.question_id) as rateTable
+
+ORDER BY rateTable.answerRate DESC, qid ASC
+LIMIT 1;
