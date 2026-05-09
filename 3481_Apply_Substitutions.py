23c4027a06e473a16323bb0c48bcd440b9f39e9c
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-05-08 22:30:18 -0700

    3481 working with manually written replace function

--- 3481_Apply_Substitutions.py
+++ 3481_Apply_Substitutions.py
@@ -1,4 +1,5 @@
 from typing import List
+import copy
 
 class Solution:
     def applySubstitutions(self, replacements: List[List[str]], text: str) -> str:
@@ -9,11 +10,26 @@ class Solution:
                 # print("before replacement:", text)
                 charToReplace, newString = pair
                 textToReplace = "%" + charToReplace + "%"
-                text = text.replace(textToReplace, newString)
+                text = manualReplace(textToReplace, newString, text)
                 # print("after replacement :", text)
         return text
-        
 
+def manualReplace(targetText, newText, bigString):
+    ans = copy.copy(bigString)
+    while targetText in ans:
+        ans = replaceFirstInstance(targetText, newText, ans)
+    return ans
+
+def replaceFirstInstance(targetText, newText, bigString):
+    ans = copy.copy(bigString)
+    for i in range(0, len(bigString) - len(targetText)+1):
+        potentialMatchingSubstring = bigString[i:i + len(targetText)]
+        if potentialMatchingSubstring == targetText:
+            ans = ans[0:i] + newText + ans[i+len(targetText):]
+            return ans
+
+# replacement = manualReplace("ab", "cdef", "ALDKRJHGIOWUHG|ab|ASD;LFGHPQOREGH|ab|ALKDFJHGKLQHRGT")    
+# print("replacement:", replacement)
 s = Solution()
 
 replacements = [["A","bce"],["B","ace"],["C","abc%B%"]]
