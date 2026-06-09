cb29b0d760a8a0738b4f79232150aeb6081b840c
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-06-09 16:36:42 -0700

    743 rewrite

--- 743_Network_Delay_Time.py
+++ 743_Network_Delay_Time.py
@@ -1,45 +1,42 @@
+# idea: Implemenet djikstra's algorithm, then look at the node 
+#       with the highest distance.
+from heapq import heapify, heappush, heappop
 from typing import List
-from heapq import heappop, heappush
 
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        adjList = {node: [] for node in range(1,n+1)}
 
-        adjList = {}
-
-        for i in range(1, n+1):
-            adjList[i] = []
-        
         for triplet in times:
             source, dest, weight = triplet
             adjList[source].append((dest, weight))
-
-
+        
         bestSoFar = {node: float('inf') for node in adjList.keys()}
         bestSoFar[k] = 0
 
-        unvisited = [(0, k)]
+        heap = [(0, k)]
         visited = set()
-        
-        while len(unvisited) > 0 and len(visited) < n:
-            distanceToCurrentNode, currentNode = heappop(unvisited)
-
-            neighbors = adjList[currentNode]
-
-            for pair in neighbors:
-                neighbor, edgeLength = pair
-                if (distanceToCurrentNode + edgeLength) < bestSoFar[neighbor]:
-                    bestSoFar[neighbor] = distanceToCurrentNode + edgeLength
-                    heappush(unvisited, (bestSoFar[neighbor], neighbor))
+        while len(heap) > 0:
+            _, currentNode = heappop(heap)
             
+            if currentNode in visited:
+                continue
+            
+            for neighbor, distance in adjList[currentNode]:
+                if bestSoFar[currentNode] + distance < bestSoFar[neighbor]:
+                    bestSoFar[neighbor] = bestSoFar[currentNode] + distance
+                    heapPair = (bestSoFar[neighbor], neighbor)
+                    heappush(heap, heapPair)
+
+
             visited.add(currentNode)
-        
-        if max(bestSoFar.values()) == float('inf'):
+
+        furthestDistance = max(bestSoFar.values())
+        if furthestDistance == float('inf'):
             return -1
-        else:
-            return max(bestSoFar.values())
         
-        
-        
+        return furthestDistance
+
 
 
 s = Solution()
@@ -47,3 +44,4 @@ times = [[2,1,1],[2,3,1],[3,4,1]]
 n = 4
 k = 2
 ans = s.networkDelayTime(times, n, k)
+print("ans:", ans)
