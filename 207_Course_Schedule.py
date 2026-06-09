34a3a30ac3fc0b2b5d9128ca569fdfaa106c0786
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-06-09 13:23:41 -0700

    207 rewrite. don't like this though...

--- 207_Course_Schedule.py
+++ 207_Course_Schedule.py
@@ -1,77 +1,47 @@
 from typing import List
-import copy
 
 class Solution:
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
-        adjList = {}
-        for i in range(0, numCourses):
-            adjList[i] = []
-        
-        for pair in prerequisites:
-            beforeCourse, afterCourse = pair
-            adjList[beforeCourse].append(afterCourse)
-        
-        allOrderings = topSort(adjList)
-        if len(allOrderings) == 0:
-            return []
-        return allOrderings[0]
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjList = {i: [] for i in range(0, numCourses)}
 
-def topSort(adjList):
-    inDegreeMap = {node: 0 for node in adjList.keys()}
+        for edge in prerequisites:
+            before, after = edge
+            adjList[before].append(after)
+        
+        unvisited = set(range(0, numCourses))
+        while len(unvisited) > 0:
+            currentNode = next(iter(unvisited))
+            result = hasCycle(currentNode, adjList, unvisited, currentPath = set())
+            if result:
+                return False
 
-    for node, neighbors in adjList.items():
-        for neighbor in neighbors:
-            inDegreeMap[neighbor] += 1
-
-    visited = set()
+        return True
     
-    orderings = topSortRecursive(inDegreeMap, adjList)
-    return orderings
-
-def topSortRecursive(inDegreeMap, adjList):
-    if len(adjList) == 1:
-        onlyKey = list(adjList.keys())[0]
-        return [[onlyKey]]
-
-    allOrderings = []
-    for node, degree in inDegreeMap.items():
-        if degree != 0:
-            continue
-
-        newAdjList = copy.deepcopy(adjList)
-        newInDegreeMap = copy.deepcopy(inDegreeMap)
-
-        for neighbor in newAdjList[node]:
-            newInDegreeMap[neighbor] -= 1
-
-        del newAdjList[node]
-        del newInDegreeMap[node]
-        recursiveResult = topSortRecursive(newInDegreeMap, newAdjList)
-
-        for ordering in recursiveResult:
-            ordering.append(node)
-            allOrderings.append(ordering)
-
-    return allOrderings
-
+def hasCycle(currentNode, adjList, unvisited, currentPath):
+    if currentNode in currentPath:
+        return True
     
+    # if the current node has been visited before, 
+    # we haven't found a cycle yet, so we're done.
+    if currentNode not in unvisited:
+        return False
+    
+    currentPath.add(currentNode)
+
+    for neighbor in adjList[currentNode]:
+        result = hasCycle(neighbor, adjList, unvisited, currentPath)
+        if result:
+            return True
+    
+    currentPath.remove(currentNode)
+    unvisited.remove(currentNode)
+    return False
+
+
 s = Solution()
 
 numCourses = 4
-prerequisites = [
-    [2,1],
-    [3,1],
-    [1,0],
-    
+prerequisites = [[0,1],[1,2],[2,3]]
 
-]
-# prerequisites = [
-#     [0,1],
-#     [1,0]
-# ]
-
-# prerequisites = [[1,0],[1,2],[0,1]]
-
-ans = s.findOrder(numCourses, prerequisites)
+ans = s.canFinish(numCourses, prerequisites)
 print("ans:", ans)
