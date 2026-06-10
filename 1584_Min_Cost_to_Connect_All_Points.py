7c522c21fcdd1012ad63af007b8d91371b532f14
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-06-10 11:52:11 -0700

    1584 done

--- 1584_Min_Cost_to_Connect_All_Points.py
+++ 1584_Min_Cost_to_Connect_All_Points.py
@@ -1,108 +1,95 @@
 from typing import List
-from heapq import *
+from heapq import heapify, heappush, heappop
 
-class Triplet():
-    def __init__(self, p1, p2, d):
-        self.p1 = p1
-        self.p2 = p2
-        self.d = d
+class UnionFind:
+    def __init__(self):
+        self.parents = {}
+        self.sizes = {}
     
-    def __lt__(self, other):
-        return self.d < other.d
+    def add(self, x):
+        if x in self.parents:
+            return
+        self.parents[x] = None
+        self.sizes[x] = 1
     
-    def __repr__(self):
-        ans = "("
-        ans += str(self.p1)
-        ans += ","
-        ans += str(self.p2)
-        ans += ", "
-        ans += str(self.d)
-        ans += ")"
-        return ans
-    
-    def __hash__(self):
-        return hash(str(self.p1) + str(self.p2) + str(self.d))
-    
+    def find(self, x):
+        currentNode = x
+        nodesSeen = []
+        while self.parents[currentNode] is not None:
+            nodesSeen.append(currentNode)
+            currentNode = self.parents[currentNode]
         
+        for node in nodesSeen:
+            self.parents[node] = currentNode
+        return currentNode
+    
+    def setSize(self, x):
+        return self.sizes[self.find(x)]
 
-def distance(point1, point2):
-    x1, y1 = point1
-    x2, y2 = point2
-    return abs(x1-x2) + abs(y1-y2)
+    def merge(self, x, y):
+        if not(x in self.parents and y in self.parents):
+            raise Exception(f"One of {x} and {y} is not in the data structure!")
+
+        if self.find(x) == self.find(y):
+            return
+
+        root_x = self.find(x)
+        root_y = self.find(y)
+
+        if self.sizes[root_x] > self.sizes[root_y]:
+            bigger, smaller = root_x, root_y
+        else:
+            bigger, smaller = root_y, root_x
+        
+        self.parents[smaller] = bigger
+        self.sizes[bigger] += self.sizes[smaller]
+    
+    def sameSet(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def neighbors(self, x):
+        root = self.find(x)
+        return set([node for node in self.parents.keys() if self.find(node) == root])
+
+    def subsets(self):
+        return {node : self.neighbors(node) for node in self.parents.keys() if self.find(node) == node}
+
+    def __repr__(self):
+        return str(self.subsets())
 
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        pairs = []
+
+        uf = UnionFind()
+        for i in range(0, len(points)):
+            uf.add(tuple(points[i]))
+            for j in range(i+1, len(points)):
+                d = manhattanDistance(points[i], points[j])
+                pairs.append((d, tuple(points[i]), tuple(points[j])))
         
+        heapify(pairs)
+        totalDistance = 0
+        mergeCount = 0
+        while len(pairs) > 0:
+            distance, point_i, point_j = heappop(pairs)
+            if uf.find(point_i) == uf.find(point_j):
+                continue
+            totalDistance += distance
+            mergeCount += 1
+            uf.merge(point_i, point_j)
+            if mergeCount == len(points):
+                break
+        return totalDistance
 
-        pointsAsTuples = [tuple(p) for p in points]
-        distancePairs = [Triplet(p1, p2, distance(p1, p2)) for p1 in pointsAsTuples for p2 in pointsAsTuples]
-        actualPairs = list(filter(lambda T: T.d != 0, distancePairs))
-        heapify(actualPairs)
-
-        inTree = set([pointsAsTuples[0]])
-        outOfTree = set(pointsAsTuples)
-        outOfTree.remove(pointsAsTuples[0])
-
-
-        # algorithm to compute MST:
-        # find the smallest existing edge that connects the current tree 
-        # to something that's not in the tree. 
-        # then, add that edge to the tree. remove from the heap. etc... 
-        # initially, all such edges are in the tree.
-        # when we choose an initial node, then we have to.
-        edges = set()
-        while len(outOfTree) > 0:
-            # point1, point2, dist = getShortestEdge(inTree, outOfTree, actualPairs)
-            triplet = getShortestEdge(inTree, outOfTree, actualPairs)
-            point1, point2, dist = triplet.p1, triplet.p2, triplet.d
-            edges.add(triplet)
-            inTree.add(point1)
-            inTree.add(point2)
-            if point1 in outOfTree:
-                outOfTree.remove(point1)
-            if point2 in outOfTree:
-                outOfTree.remove(point2)
-            
-            # print("inTree:", inTree)
-            # print("edges:", edges)
-
-
-        ans = 0
-        for triplet in edges:
-            # print("edge:", tripl)
-            ans += triplet.d
-        # print("ans:", ans)
-        return ans
-            
-
-def getShortestEdge(inTree, outOfTree, pairs):
-    triplet = heappop(pairs)
-    rejects = [triplet]
-    # print("inTree:", inTree)
-    # print("outOfTree:", outOfTree)
-    # print("pairs:", pairs)
-    
-    while not(connectsToOutside(triplet, inTree, outOfTree)):
-        triplet = heappop(pairs)
-        rejects.append(triplet)
-    
-    for r in rejects:
-        heappush(pairs, r)
-
-    return triplet
-    
-
-    
-
-
-
-def connectsToOutside(triplet, inTree, outOfTree):
-    p1, p2 = triplet.p1, triplet.p2
-    return (p1 in inTree) == (p2 in outOfTree)
         
+def manhattanDistance(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return (abs(x1-x2) + abs(y1-y2))
 
 
-
-points = [[0,0],[2,2],[3,10],[5,2],[7,0]]
+points = [[3,12],[-2,5],[-4,1]]
 mySol = Solution()
-mySol.minCostConnectPoints(points)
+ans = mySol.minCostConnectPoints(points)
+print("ans:", ans)
