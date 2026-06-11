ec3e4191dd14be42c3e29ff1f059baa6c616899b
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-06-10 22:16:09 -0700

    codeforces optimizations

--- codeforces_448_C.py
+++ codeforces_448_C.py
@@ -1,27 +1,37 @@
 numPlanks = 5
 plankHeights = [2, 2, 1, 2, 1]
 
-def numStrokes(plankHeights):
-    if len(plankHeights) == 0:
+def numStrokes(plankHeights, memo = None):
+    if memo is None:
+        memo = {}
+    
+    memoKey = tuple(plankHeights)
+    if memoKey in memo:
+        return memo[memoKey]
+    if len(plankHeights) == 0 or len(plankHeights) == 1:
         return 1
-    bestVertical = verticalStroke(plankHeights)
-    bestHorizontal = horizontalStroke(plankHeights)
-    return 1 + min(bestVertical, bestHorizontal)
+    
 
-def verticalStroke(plankHeights):
+    bestVertical = verticalStroke(plankHeights, memo)
+    bestHorizontal = horizontalStroke(plankHeights, memo)
+    ans = 1 + min(bestVertical, bestHorizontal)
+    memo[memoKey] = ans
+    return ans
+
+def verticalStroke(plankHeights, memo):
     answers = []
     for i in range(0, len(plankHeights)):
         remaining = plankHeights[0:i] + plankHeights[i+1:]
-        answers.append(numStrokes(remaining))
+        answers.append(numStrokes(remaining, memo))
     return min(answers)
 
 
-def horizontalStroke(plankHeights):
+def horizontalStroke(plankHeights, memo):
     newHeights = [h - 1 for h in plankHeights]
     remainingPlanks = list(filter(lambda x: x != 0, newHeights))
     if len(remainingPlanks) == 0:
         return 1
-    return numStrokes(remainingPlanks)
+    return numStrokes(remainingPlanks, memo)
 
 
 ans = numStrokes(plankHeights)
