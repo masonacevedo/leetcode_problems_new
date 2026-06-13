ccfbf6d4105f41094b19aabe70614c503f19b15d
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-06-12 17:32:40 -0700

    5 edge cases and performance optimization

--- 5_Longest_Palindromic_Substring.py
+++ 5_Longest_Palindromic_Substring.py
@@ -2,29 +2,41 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         if len(s) == 1:
             return s
+        if len(s) == 2:
+            return s if s[0] == s[1] else s[0]
         DPTable = {}
         for i in range(0, len(s)):
-            DPTable[(i,i)] = (s[i], True)
-            DPTable[(i+1,i)] = ("", True)
+            DPTable[(i,i)] = ((i, i), True)
+            DPTable[(i+1,i)] = ((i+1, i), True)
         coords = (0,1)
         while coords is not None:
             i, j = coords[0], coords[1]
             DPKey = (i,j)
             cornerEntry = DPTable[(i+1, j-1)]
-            cornerLength, cornerIsPalindrome = cornerEntry
+            cornerCoords, cornerIsPalindrome = cornerEntry
             if s[i] == s[j] and cornerIsPalindrome:
-                DPTable[DPKey] = (s[i:j+1], True)
+                DPTable[DPKey] = ((i, j+1,), True)
             else:
                 aboveEntry = DPTable[(i+1, j)][0]
                 rightEntry = DPTable[(i, j-1)][0]
-                if len(aboveEntry) > len(rightEntry):
+
+                aboveEntryLength = aboveEntry[1] - aboveEntry[0]
+                rightEntryLength = rightEntry[1] - rightEntry[0]
+
+                if aboveEntryLength > rightEntryLength:
                     longer = aboveEntry
                 else:
                     longer = rightEntry
+
                 DPTable[DPKey] = (longer, False)
             coords = nextEntry(coords[0], coords[1], len(s))
         
-        return DPTable[(0, len(s)-1)][0]
+        bestEntry, _ = DPTable[(0, len(s)-1)]
+        ans = s[bestEntry[0]:bestEntry[1]]
+        if len(ans) == 0:
+            return s[0]
+        else:
+            return ans
 
 
 def nextEntry(i,j, numRows):
@@ -48,3 +60,11 @@ assert(sol.longestPalindrome(s) == "racecar")
 
 s = "cmmrracelnclsbtdmuxtfiyahrvxuwreyorosyqapfpnsntommsujibzwhgugwtvxsdsltiiyymiofbslwbwevmjrsbbssicnxptvwmsmiifypoujftxylpyvirfueagprfyyydxeiftathaygmolkcwoaavmdmjsuwoibtuqoewaexihispsshwnsurjopdwttlzyqdbkypvjsbuidsdnpgklhwfnqdvlffcysnxeywvwvblatmxbflnuykhfhjptenhcxqinomlwalvlezefqybpuepbnymzlruuirpiatqgjgcnfmrlzshauoxuoqopcikogfwpssjdeplytcapmujyvgtfmmolnuadpwblgmcaututcrwsqrlpaaqobjfnhudmsulztbdkxpfejavastxejtpbqfftdtcdhvtpbzfuqcwkxtldtjycreimiujtxudtmokcoebhodbkgkgxjzrgyuqhozqtidltodlkziyofdeszwiobkwesdijxbbagguxvofvtphqxgvidqfkljufgubjmjllxoanbizwtedykwmneaosopynzlzvrlqcmyaahdcagfatlhwtgqxsyxwjhexfiplwtrtydjzrsysrcwszlntwrpgfedhgjzhztffqnjotlfudvczwfkhuwmdzcqgrmfttwaxocohtuscdxwfvhcymjpkqexusdaccw"
 print(sol.longestPalindrome(s))
+
+s = "abcda"
+assert(len(sol.longestPalindrome(s)) == 1)
+
+s = "aaaa"
+print(sol.longestPalindrome(s))
+
+s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
