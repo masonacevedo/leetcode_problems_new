b7009b67de0decf7f6bdcbece03f201ce835079a
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-06-25 22:50:52 -0700

    3737 o(n^2) solution with prefixes/suffixes

--- 3737_Count_Subarrays_With_Majority_Element_I.py
+++ 3737_Count_Subarrays_With_Majority_Element_I.py
@@ -2,12 +2,50 @@ from typing import List
 
 class Solution:
     def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
+
+        totalTargets = nums.count(target)
+        # print("nums:")
+        # print(nums)
+        prefixCounts = [0]
+        count = 0
+        for i in range(0, len(nums)):
+            if nums[i] == target:
+                count += 1
+            prefixCounts.append(count)
+        
+
+        suffixCounts = [0]
+        count = 0
+        for i in reversed(range(0, len(nums))):
+            if nums[i] == target:
+                count += 1
+            suffixCounts.append(count)
+        suffixCounts = list(reversed(suffixCounts))
         ans = 0
+        # print("prefixCounts:")
+        # print(prefixCounts)
+        # print("suffixCounts:")
+        # print(suffixCounts)
+        # breakpoint()
         for i in range(0, len(nums)):
             for j in range(i, len(nums)+1):
-                subarray = nums[i:j]
-                if majorityMatch(subarray, target):
+                # print("subarray:")
+                # print(nums[i:j])
+                # print("potential answer?")
+                # print(totalTargets - prefixCounts[i] - suffixCounts[j])
+                # print("array length:")
+                # print(j - i)
+                numTargets = totalTargets - prefixCounts[i] - suffixCounts[j]
+                if numTargets > ((j-i)//2):
                     ans += 1
+                #     print("increasing ans!")
+                # else:
+                #     print("not increasing ans...")
+                
+                
+                # breakpoint()
+
+
         return ans
 
 def majorityMatch(subarray, target):
