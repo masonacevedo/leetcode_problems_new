04342bdf4dc4be1d28f6b4a0d5d23bbd04b62c19
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-07-02 13:28:15 -0700

    3286 cleanup

--- 3286_Find_a_Safe_Walk_Through_a_Grid.py
+++ 3286_Find_a_Safe_Walk_Through_a_Grid.py
@@ -12,27 +12,24 @@ class Solution:
         queue = deque([start])
         seenBefore = set()
         while len(queue) > 0:
-            coords, he = queue.popleft()
-            # breakpoint()
-            if tuple(coords + [he]) in seenBefore:
+            coords, h = queue.popleft()
+
+            if tuple(coords + [h]) in seenBefore:
                 continue
-            # print(len(queue))
-            # breakpoint()
-            if coords == [len(grid)-1, len(grid[0])-1] and he >= 1:
-                # print("returning true!")
-                # breakpoint()
+
+            if coords == [len(grid)-1, len(grid[0])-1] and h >= 1:
                 return True
-            if he >= 0:
+
+            if h >= 0:
                 for neighbor in getNeighbors(coords, grid):
-                    
 
                     nRow, nCol = neighbor
                     if grid[nRow][nCol] == 1:
-                        queue.append((neighbor, he-1))
+                        queue.append((neighbor, h-1))
                     else:
-                        queue.append((neighbor, he))
-            
-            seenBefore.add(tuple(coords + [he]))
+                        queue.append((neighbor, h))
+
+            seenBefore.add(tuple(coords + [h]))
         
         return False
 
@@ -42,20 +39,20 @@ def getNeighbors(coords, grid):
     botNeighbor = [row+1, col]
     leftNeighbor = [row, col-1]
     rightNeighbor = [row, col+1]
-    
+
     neighbors = []
     if row > 0:
         neighbors.append(topNeighbor)
-    
+
     if col > 0:
         neighbors.append(leftNeighbor)
-    
+
     if row < len(grid)-1:
         neighbors.append(botNeighbor)
-    
+
     if col < len(grid[0]) - 1:
         neighbors.append(rightNeighbor)
-    
+
     return neighbors
 
 
