10141f30d8d1b35509e0db65ba0ac1b3a59f8e33
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-07-01 23:45:22 -0700

    3286 done

---
+++ 3286_Find_a_Safe_Walk_Through_a_Grid.py
@@ -0,0 +1,68 @@
+from typing import List
+from collections import deque
+
+class Solution:
+    def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
+        
+
+        start = ([0,0], health)
+
+        queue = deque([start])
+        seenBefore = set()
+        while len(queue) > 0:
+            coords, he = queue.popleft()
+            # breakpoint()
+            if coords == [len(grid)-1, len(grid[0])-1] and he >= 0:
+                return True
+            
+            for neighbor in getNeighbors(coords, grid):
+                if tuple(neighbor) in seenBefore:
+                    continue
+                
+                nRow, nCol = neighbor
+                if grid[nRow][nCol] == 1:
+                    queue.append((neighbor, he-1))
+                else:
+                    queue.append((neighbor, he))
+            
+            seenBefore.add(tuple(coords))
+        
+        return False
+
+def getNeighbors(coords, grid):
+    row, col = coords
+    topNeighbor = [row-1, col]
+    botNeighbor = [row+1, col]
+    leftNeighbor = [row, col-1]
+    rightNeighbor = [row, col+1]
+    
+    neighbors = []
+    if row > 0:
+        neighbors.append(topNeighbor)
+    
+    if col > 0:
+        neighbors.append(leftNeighbor)
+    
+    if row < len(grid)-1:
+        neighbors.append(botNeighbor)
+    
+    if col < len(grid[0]) - 1:
+        neighbors.append(rightNeighbor)
+    
+    return neighbors
+
+
+
+        
+        
+
+
+s = Solution()
+
+grid = [[0,1,0,0,0],
+        [0,1,0,1,0],
+        [0,0,0,1,0]]
+health = 1
+
+ans = s.findSafeWalk(grid, health)
+print("ans:", ans)
